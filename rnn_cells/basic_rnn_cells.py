Fixed batch normalization. The gain and bias of the normalization layers were not being stored.

rnn_cells/basic_rnn_cells.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 # Implementation from nn._functions.rnn.py
 def BasicLSTMCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None,
-                  activation=F.tanh, layer_norm=False):
+                  activation=F.tanh, lst_layer_norm=None):
     '''
     Parameters of a basic LSTM cell
     :param forget_bias: float, the bias added to the forget gates. The biases are for the forget gate in order to
@@ -18,18 +18,11 @@ def BasicLSTMCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None,
 
     ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
 
-    # TODO: check the input size for the BatchNorm1d
-    if layer_norm:
-        if ingate.is_cuda:
-            ingate = nn.BatchNorm1d(ingate.shape[1]).cuda()(ingate.contiguous())
-            forgetgate = nn.BatchNorm1d(forgetgate.shape[1]).cuda()(forgetgate.contiguous())
-            cellgate = nn.BatchNorm1d(cellgate.shape[1]).cuda()(cellgate.contiguous())
-            outgate = nn.BatchNorm1d(outgate.shape[1]).cuda()(outgate.contiguous())
-        else:
-            ingate = nn.BatchNorm1d(ingate.shape[1])(ingate.contiguous())
-            forgetgate = nn.BatchNorm1d(forgetgate.shape[1])(forgetgate.contiguous())
-            cellgate = nn.BatchNorm1d(cellgate.shape[1])(cellgate.contiguous())
-            outgate = nn.BatchNorm1d(outgate.shape[1])(outgate.contiguous())
+    if lst_layer_norm:
+        ingate = lst_layer_norm[0](ingate.contiguous())
+        forgetgate = lst_layer_norm[1](forgetgate.contiguous())
+        cellgate = lst_layer_norm[2](cellgate.contiguous())
+        outgate = lst_layer_norm[3](outgate.contiguous())
 
     ingate = F.sigmoid(ingate)
     forgetgate = F.sigmoid(forgetgate)
@@ -46,7 +39,7 @@ def BasicLSTMCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None,
 
 # Implementation from nn._functions.rnn.py
 def BasicGRUCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None,
-                  activation=F.tanh, layer_norm=False):
+                  activation=F.tanh, lst_layer_norm=None):
     '''
     Parameters of a basic GRU cell
     :param forget_bias: float, the bias added to the forget gates. The biases are for the forget gate in order to
@@ -60,16 +53,11 @@ def BasicGRUCell(input, hidden, w_ih, w_hh, b_ih=None, b_hh=None,
     i_r, i_i, i_n = gi.chunk(3, 1)
     h_r, h_i, h_n = gh.chunk(3, 1)
 
-    # TODO: check the input size for the BatchNorm1d
     resetgate_tmp = i_r + h_r
     inputgate_tmp = i_i + h_i
-    if layer_norm:
-        if inputgate_tmp.is_cuda:
-            resetgate_tmp = nn.BatchNorm1d(resetgate_tmp.shape[1]).cuda()(resetgate_tmp.contiguous())
-            inputgate_tmp = nn.BatchNorm1d(inputgate_tmp.shape[1]).cuda()(inputgate_tmp.contiguous())
-        else:
-            resetgate_tmp = nn.BatchNorm1d(resetgate_tmp.shape[1])(resetgate_tmp.contiguous())
-            inputgate_tmp = nn.BatchNorm1d(inputgate_tmp.shape[1])(inputgate_tmp.contiguous())
+    if lst_layer_norm:
+        resetgate_tmp = lst_layer_norm[0](resetgate_tmp.contiguous())
+        inputgate_tmp = lst_layer_norm[1](inputgate_tmp.contiguous())
 
     resetgate = F.sigmoid(resetgate_tmp)
     inputgate = F.sigmoid(inputgate_tmp)

rnn_cells/custom_cells.py
@@ -50,6 +50,7 @@ class CCellBase(RNNCellBase):
         self.reset_parameters()
         self.activation = activation
         self.layer_norm = layer_norm
+        self.lst_bnorm_rnn = None
 
     def reset_parameters(self):
         stdv = 1.0 / math.sqrt(self.hidden_size)
@@ -83,6 +84,13 @@ class CCellBaseLSTM(CCellBase):
             self.check_forward_hidden(input, hx[0], '[0]')
             self.check_forward_hidden(input, hx[1], '[1]')
 
+        # Initialize batchnorm layers
+        if self.layer_norm and self.lst_bnorm_rnn is None:
+            # Create gain and bias for input_gate, new_input, forget_gate, output_gate
+            self.lst_bnorm_rnn = torch.nn.ModuleList([nn.BatchNorm1d(self.hidden_size) for i in np.arange(4)])
+            if input.is_cuda:
+                self.lst_bnorm_rnn = self.lst_bnorm_rnn.cuda()
+
         lst_output = []
         for t in np.arange(sequence_length):
             hx = self.cell(
@@ -90,7 +98,7 @@ class CCellBaseLSTM(CCellBase):
                 self.weight_ih[0], self.weight_hh[0],
                 self.bias_ih[0], self.bias_hh[0],
                 activation=self.activation,
-                layer_norm=self.layer_norm
+                lst_layer_norm=self.lst_bnorm_rnn
             )
             lst_output.append(hx[0])
         output = torch.stack(lst_output)
@@ -120,6 +128,13 @@ class CCellBaseGRU(CCellBase):
         else:
             self.check_forward_hidden(input, hx, '[0]')
 
+        # Initialize batchnorm layers
+        if self.layer_norm and self.lst_bnorm_rnn is None:
+            # Create gain and bias for reset_gate and update_gate
+            self.lst_bnorm_rnn = torch.nn.ModuleList([nn.BatchNorm1d(self.hidden_size) for i in np.arange(2)])
+            if input.is_cuda:
+                self.lst_bnorm_rnn = self.lst_bnorm_rnn.cuda()
+
         lst_output = []
         for t in np.arange(sequence_length):
             hx = self.cell(
@@ -127,7 +142,7 @@ class CCellBaseGRU(CCellBase):
                 self.weight_ih[0], self.weight_hh[0],
                 self.bias_ih[0], self.bias_hh[0],
                 activation=self.activation,
-                layer_norm=self.layer_norm
+                lst_layer_norm=self.lst_bnorm_rnn
             )
             lst_output.append(hx)
         output = torch.stack(lst_output)
@@ -183,6 +198,18 @@ class CCellBaseSkipLSTM(CCellBase):
                 self.check_forward_hidden(input, hx[0], '[0]')
                 self.check_forward_hidden(input, hx[1], '[1]')
 
+        # Initialize batchnorm layers
+        if self.layer_norm and self.lst_bnorm_rnn is None:
+            self.lst_bnorm_rnn = []
+            for i in np.arange(self.num_layers):
+                # Create gain and bias for input_gate, new_input, forget_gate, output_gate
+                lst_bnorm_rnn_tmp = torch.nn.ModuleList([nn.BatchNorm1d(self.hidden_size) for i in np.arange(4)])
+                if input.is_cuda:
+                    lst_bnorm_rnn_tmp = lst_bnorm_rnn_tmp.cuda()
+                self.lst_bnorm_rnn.append(lst_bnorm_rnn_tmp)
+            self.lst_bnorm_rnn = torch.nn.ModuleList(self.lst_bnorm_rnn)
+
+
         lst_output = []
         lst_update_gate = []
         for t in np.arange(sequence_length):
@@ -191,7 +218,7 @@ class CCellBaseSkipLSTM(CCellBase):
                 self.weight_ih, self.weight_hh, self.weight_uh,
                 self.bias_ih, self.bias_hh, self.bias_uh,
                 activation=self.activation,
-                layer_norm=self.layer_norm
+                lst_layer_norm=self.lst_bnorm_rnn
             )
             new_h, update_gate = output
             lst_output.append(new_h)
@@ -246,6 +273,17 @@ class CCellBaseSkipGRU(CCellBase):
             else:
                 self.check_forward_hidden(input, hx[0], '[0]')
 
+        # Initialize batchnorm layers
+        if self.layer_norm and self.lst_bnorm_rnn is None:
+            self.lst_bnorm_rnn = []
+            for i in np.arange(self.num_layers):
+                # Create gain and bias for input_gate, new_input, forget_gate, output_gate
+                lst_bnorm_rnn_tmp = torch.nn.ModuleList([nn.BatchNorm1d(self.hidden_size) for i in np.arange(2)])
+                if input.is_cuda:
+                    lst_bnorm_rnn_tmp = lst_bnorm_rnn_tmp.cuda()
+                self.lst_bnorm_rnn.append(lst_bnorm_rnn_tmp)
+            self.lst_bnorm_rnn = torch.nn.ModuleList(self.lst_bnorm_rnn)
+
         lst_output = []
         lst_update_gate = []
         for t in np.arange(sequence_length):
@@ -254,7 +292,7 @@ class CCellBaseSkipGRU(CCellBase):
                 self.weight_ih, self.weight_hh, self.weight_uh,
                 self.bias_ih, self.bias_hh, self.bias_uh,
                 activation=self.activation,
-                layer_norm=self.layer_norm
+                lst_layer_norm=self.lst_bnorm_rnn
             )
             new_h, update_gate = output
             lst_output.append(new_h)

rnn_cells/skip_rnn_cells.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import numpy as np
 
 def SkipLSTMCell(input, hidden, num_layers, w_ih, w_hh, w_uh,b_ih=None, b_hh=None, b_uh=None,
-                  activation=F.tanh, layer_norm=False):
+                  activation=F.tanh, lst_layer_norm=None):
     if num_layers != 1:
         raise RuntimeError("wrong num_layers: got {}, expected {}".format(num_layers, 1))
     w_ih, w_hh = w_ih[0], w_hh[0]
@@ -17,17 +17,11 @@ def SkipLSTMCell(input, hidden, num_layers, w_ih, w_hh, w_uh,b_ih=None, b_hh=Non
 
     ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
 
-    if layer_norm:
-        if ingate.is_cuda:
-            ingate = nn.BatchNorm1d(ingate.shape[1]).cuda()(ingate.contiguous())
-            forgetgate = nn.BatchNorm1d(forgetgate.shape[1]).cuda()(forgetgate.contiguous())
-            cellgate = nn.BatchNorm1d(cellgate.shape[1]).cuda()(cellgate.contiguous())
-            outgate = nn.BatchNorm1d(outgate.shape[1]).cuda()(outgate.contiguous())
-        else:
-            ingate = nn.BatchNorm1d(ingate.shape[1])(ingate.contiguous())
-            forgetgate = nn.BatchNorm1d(forgetgate.shape[1])(forgetgate.contiguous())
-            cellgate = nn.BatchNorm1d(cellgate.shape[1])(cellgate.contiguous())
-            outgate = nn.BatchNorm1d(outgate.shape[1])(outgate.contiguous())
+    if lst_layer_norm:
+        ingate = lst_layer_norm[0][0](ingate.contiguous())
+        forgetgate = lst_layer_norm[0][1](forgetgate.contiguous())
+        cellgate = lst_layer_norm[0][2](cellgate.contiguous())
+        outgate = lst_layer_norm[0][3](outgate.contiguous())
 
     ingate = F.sigmoid(ingate)
     forgetgate = F.sigmoid(forgetgate)
@@ -57,7 +51,7 @@ def SkipLSTMCell(input, hidden, num_layers, w_ih, w_hh, w_uh,b_ih=None, b_hh=Non
 ######################################################
 
 def SkipGRUCell(input, state, num_layers, w_ih, w_hh, w_uh,b_ih=None, b_hh=None, b_uh=None,
-                  activation=F.tanh, layer_norm=False):
+                  activation=F.tanh, lst_layer_norm=None):
 
     if num_layers != 1:
         raise RuntimeError("wrong num_layers: got {}, expected {}".format(num_layers, 1))
@@ -74,13 +68,10 @@ def SkipGRUCell(input, state, num_layers, w_ih, w_hh, w_uh,b_ih=None, b_hh=None,
 
     resetgate_tmp = i_r + h_r
     inputgate_tmp = i_i + h_i
-    if layer_norm:
-        if inputgate_tmp.is_cuda:
-            resetgate_tmp = nn.BatchNorm1d(resetgate_tmp.shape[1]).cuda()(resetgate_tmp.contiguous())
-            inputgate_tmp = nn.BatchNorm1d(inputgate_tmp.shape[1]).cuda()(inputgate_tmp.contiguous())
-        else:
-            resetgate_tmp = nn.BatchNorm1d(resetgate_tmp.shape[1])(resetgate_tmp.contiguous())
-            inputgate_tmp = nn.BatchNorm1d(inputgate_tmp.shape[1])(inputgate_tmp.contiguous())
+
+    if lst_layer_norm:
+        resetgate_tmp = lst_layer_norm[0][0](resetgate_tmp.contiguous())
+        inputgate_tmp = lst_layer_norm[0][1](inputgate_tmp.contiguous())
 
     resetgate = F.sigmoid(resetgate_tmp)
     inputgate = F.sigmoid(inputgate_tmp)
@@ -106,7 +97,7 @@ def SkipGRUCell(input, state, num_layers, w_ih, w_hh, w_uh,b_ih=None, b_hh=None,
 ######################################################
 
 def MultiSkipLSTMCell(input, state, num_layers, w_ih, w_hh, w_uh,b_ih=None, b_hh=None, b_uh=None,
-                  activation=F.tanh, layer_norm=False):
+                  activation=F.tanh, lst_layer_norm=None):
 
     _, _ , update_prob_prev, cum_update_prob_prev = state[-1]
     cell_input = input
@@ -119,17 +110,11 @@ def MultiSkipLSTMCell(input, state, num_layers, w_ih, w_hh, w_uh,b_ih=None, b_hh
 
         ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
 
-        if layer_norm:
-            if ingate.is_cuda:
-                ingate = nn.BatchNorm1d(ingate.shape[1]).cuda()(ingate.contiguous())
-                forgetgate = nn.BatchNorm1d(forgetgate.shape[1]).cuda()(forgetgate.contiguous())
-                cellgate = nn.BatchNorm1d(cellgate.shape[1]).cuda()(cellgate.contiguous())
-                outgate = nn.BatchNorm1d(outgate.shape[1]).cuda()(outgate.contiguous())
-            else:
-                ingate = nn.BatchNorm1d(ingate.shape[1])(ingate.contiguous())
-                forgetgate = nn.BatchNorm1d(forgetgate.shape[1])(forgetgate.contiguous())
-                cellgate = nn.BatchNorm1d(cellgate.shape[1])(cellgate.contiguous())
-                outgate = nn.BatchNorm1d(outgate.shape[1])(outgate.contiguous())
+        if lst_layer_norm:
+            ingate = lst_layer_norm[idx][0](ingate.contiguous())
+            forgetgate = lst_layer_norm[idx][1](forgetgate.contiguous())
+            cellgate = lst_layer_norm[idx][2](cellgate.contiguous())
+            outgate = lst_layer_norm[idx][3](outgate.contiguous())
 
         ingate = F.sigmoid(ingate)
         forgetgate = F.sigmoid(forgetgate)
@@ -169,7 +154,7 @@ def MultiSkipLSTMCell(input, state, num_layers, w_ih, w_hh, w_uh,b_ih=None, b_hh
 ######################################################
 
 def MultiSkipGRUCell(input, state, num_layers, w_ih, w_hh, w_uh,b_ih=None, b_hh=None, b_uh=None,
-                  activation=F.tanh, layer_norm=False):
+                  activation=F.tanh, lst_layer_norm=None):
 
     _ , update_prob_prev, cum_update_prob_prev = state[-1]
     cell_input = input
@@ -186,13 +171,9 @@ def MultiSkipGRUCell(input, state, num_layers, w_ih, w_hh, w_uh,b_ih=None, b_hh=
 
         resetgate_tmp = i_r + h_r
         inputgate_tmp = i_i + h_i
-        if layer_norm:
-            if inputgate_tmp.is_cuda:
-                resetgate_tmp = nn.BatchNorm1d(resetgate_tmp.shape[1]).cuda()(resetgate_tmp.contiguous())
-                inputgate_tmp = nn.BatchNorm1d(inputgate_tmp.shape[1]).cuda()(inputgate_tmp.contiguous())
-            else:
-                resetgate_tmp = nn.BatchNorm1d(resetgate_tmp.shape[1])(resetgate_tmp.contiguous())
-                inputgate_tmp = nn.BatchNorm1d(inputgate_tmp.shape[1])(inputgate_tmp.contiguous())
+        if lst_layer_norm:
+            resetgate_tmp = lst_layer_norm[idx][0](resetgate_tmp.contiguous())
+            inputgate_tmp = lst_layer_norm[idx][1](inputgate_tmp.contiguous())
 
         resetgate = F.sigmoid(resetgate_tmp)
         inputgate = F.sigmoid(inputgate_tmp)